3187afec765988d915e2215cf76eb00d8dbc8e41
Hyokune <19813076+Hyokune@users.noreply.github.com>
2020-05-20 23:31:51 +1200

    Added 2 - 2839 answer

---
+++ LEVEL 9 - 수학 1/2 - 2839.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int totalBagCount = 0, totalWeight;
+    cin >> totalWeight;
+    
+    while (true)
+    {
+        if (totalWeight % 5 == 0)
+        {
+            totalBagCount += totalWeight / 5;
+            break;
+        }
+        
+        totalBagCount++;
+        totalWeight -= 3;
+        
+        if (totalWeight < 0)
+        {
+            totalBagCount = -1;
+            break;
+      }
+    }
+    
+    cout << totalBagCount;
+	
+    return 0;
+}
